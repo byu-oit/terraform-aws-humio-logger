0c32673060f8ef38650a56cbfd3a301b2ca88fab
Blake Morgan <blake_morgan@byu.edu>
2021-07-19 15:50:15 -0400

    Update CI names

--- examples/ci/ci.tf
+++ examples/ci/ci.tf
@@ -18,18 +18,18 @@ module "ci_test" {
 
   source                    = "../../"
   app_env                   = "dev"
-  app_name                  = "humio-logger-dev"
+  app_name                  = "humio-logger-ci"
   humio_ingest_token        = "4788f2d0-b72d-484d-944d-830aba612207"
   log_group_arns            = [aws_cloudwatch_log_group.humio_logger.arn]
   log_group_names           = [aws_cloudwatch_log_group.humio_logger.name]
   private_vpn_subnet_ids    = module.acs_vpn.private_subnet_ids
   role_permissions_boundary = module.acs_vpn.role_permissions_boundary.arn
-  sub_idx_nm                = "payments"
+  sub_idx_nm                = "terraform"
   vpn_vpc_id                = module.acs_vpn.vpc.id
 }
 
 resource "aws_cloudwatch_log_group" "humio_logger" {
-  name = "humio-logger-dev-logs"
+  name = "humio-logger-ci-logs"
 }
 
 module "acs_vpn" {
