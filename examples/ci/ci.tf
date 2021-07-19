9ab9b3adcfcc4d6ad3c95bfb409d197832f24e90
Blake Morgan <blake_morgan@byu.edu>
2021-07-19 15:52:15 -0400

    Keep CloudWatch group

--- examples/ci/ci.tf
+++ examples/ci/ci.tf
@@ -20,15 +20,15 @@ module "ci_test" {
   app_env                   = "dev"
   app_name                  = "humio-logger-ci"
   humio_ingest_token        = "4788f2d0-b72d-484d-944d-830aba612207"
-  log_group_arns            = [aws_cloudwatch_log_group.humio_logger.arn]
-  log_group_names           = [aws_cloudwatch_log_group.humio_logger.name]
+  log_group_arns            = [data.aws_cloudwatch_log_group.humio_logger.arn]
+  log_group_names           = [data.aws_cloudwatch_log_group.humio_logger.name]
   private_vpn_subnet_ids    = module.acs_vpn.private_subnet_ids
   role_permissions_boundary = module.acs_vpn.role_permissions_boundary.arn
   sub_idx_nm                = "terraform"
   vpn_vpc_id                = module.acs_vpn.vpc.id
 }
 
-resource "aws_cloudwatch_log_group" "humio_logger" {
+data "aws_cloudwatch_log_group" "humio_logger" {
   name = "humio-logger-ci-logs"
 }
 
