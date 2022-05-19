141daba5f966f5440939bf604a9cd74bb7fa7dbf
Spencer Tuft <stuft2@byu.edu>
2022-05-19 11:09:46 -0600

    fix: add iam capability

--- main.tf
+++ main.tf
@@ -32,6 +32,7 @@ resource "aws_security_group" "logging" {
 resource "aws_cloudformation_stack" "cloudwatch2humio" {
   name          = "${var.app_name}-cloudwatch2humio"
   template_body = data.local_file.cloudformation.content
+  capabilities  = ["CAPABILITY_IAM"]
   parameters = {
     HumioProtocol                         = var.humio_protocol
     HumioHost                             = var.humio_host
