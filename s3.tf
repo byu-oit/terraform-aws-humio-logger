f605114ed449cd5393678ee690abc6464b872763
Spencer Tuft <stuft2@byu.edu>
2022-12-09 13:52:16 -0700

    fix: add explicit dependency on buckets

--- s3.tf
+++ s3.tf
@@ -24,6 +24,7 @@ resource "aws_s3_bucket" "cloudwatch2humio_source_code_bucket" {
 
 resource "aws_s3_bucket_object" "cloudwatch2humio_source_code_object" {
   count       = local.create_source_storage ? 1 : 0
+  depends_on  = [aws_s3_bucket.cloudwatch2humio_source_code_bucket]
   bucket      = aws_s3_bucket.cloudwatch2humio_source_code_bucket[0].bucket
   key         = local.archive_name
   source      = local.archive_path
@@ -31,6 +32,7 @@ resource "aws_s3_bucket_object" "cloudwatch2humio_source_code_object" {
 }
 
 data "aws_s3_bucket_object" "cloudwatch2humio_source_code_object" {
-  bucket = local.create_source_storage ? aws_s3_bucket.cloudwatch2humio_source_code_bucket[0].bucket : local.bucket_name
-  key    = local.archive_name
+  depends_on = [aws_s3_bucket_object.cloudwatch2humio_source_code_object]
+  bucket     = local.create_source_storage ? aws_s3_bucket.cloudwatch2humio_source_code_bucket[0].bucket : local.bucket_name
+  key        = local.archive_name
 }
